95e719bc17f2fa400cfc70b887d5d92ad7341a30
KenanMuradov <maljerov.1999@gmail.com>
2023-02-15 20:57:41 +0300

    Final

--- FileCopierThread/MainWindow.xaml.cs
+++ FileCopierThread/MainWindow.xaml.cs
@@ -21,14 +21,16 @@ namespace FileCopierThread
 {
     public partial class MainWindow : Window
     {
-        private readonly Thread thread;
-        private string[] pathes;
+        private Thread thread;
+        public string FilePath { get; set; }
+        public string DestinationPath { get; set; }
+        private bool isStopped = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            pathes = new string[2];
             thread = new Thread(CopyMethod);
+            DataContext = this;
         }
 
         private void btnFrom_Click(object sender, RoutedEventArgs e)
@@ -38,9 +40,8 @@ namespace FileCopierThread
 
             var result = dialog.ShowDialog();
 
-            if(result == true)
+            if (result == true)
             {
-                pathes[0] = dialog.FileName;
                 txtFrom.Text = dialog.FileName;
                 return;
             }
@@ -50,67 +51,119 @@ namespace FileCopierThread
 
         private void btnTo_Click(object sender, RoutedEventArgs e)
         {
-            VistaFolderBrowserDialog dialog= new VistaFolderBrowserDialog();
+            FileDialog dialog = new SaveFileDialog();
 
             var result = dialog.ShowDialog();
 
             if (result == true)
             {
-                pathes[1] = dialog.SelectedPath;
-                txtTo.Text = dialog.SelectedPath;
+                txtTo.Text = dialog.FileName;
             }
         }
 
         private void btnCopy_Click(object sender, RoutedEventArgs e)
         {
-            if(string.IsNullOrWhiteSpace(txtFrom.Text) || string.IsNullOrWhiteSpace(txtTo.Text))
+            if (string.IsNullOrWhiteSpace(FilePath) || string.IsNullOrWhiteSpace(DestinationPath))
             {
                 MessageBox.Show("Please enter all information");
                 return;
-            }    
+            }
 
-            if(!File.Exists(txtFrom.Text) || !Directory.Exists(txtTo.Text)) 
+            if (!File.Exists(FilePath))
             {
                 MessageBox.Show("Entered wrong file or folder path");
                 return;
             }
 
+            if (!DestinationPath.Contains(".txt"))
+                DestinationPath += ".txt";
 
+
+            btnCopy.IsEnabled = false;
             thread.Start();
 
         }
 
 
-        private async void CopyMethod()
+        private void CopyMethod()
         {
-            await Dispatcher.InvokeAsync(() =>
+            Dispatcher.Invoke(() => Progressbar.Value = 0);
+
+            using (FileStream fsRead = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.None, 4096, true))
             {
-                FileInfo file = new FileInfo(txtFrom.Text);
-                DirectoryInfo dir = new DirectoryInfo(txtTo.Text);
-                using (FileStream fsRead = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+                using (FileStream fsWrite = new FileStream(DestinationPath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
                 {
-                    using (FileStream fsWrite = new FileStream($"{dir.FullName}/{file.Name}", FileMode.Create, FileAccess.Write))
+                    var fileSize = fsRead.Length;
+                    byte[] buffer = new byte[70];
+                    var copiedBytes = 0L;
+
+                    do
                     {
-                        var len = 10;
-                        var fileSize = fsRead.Length;
-                        byte[] buffer = new byte[len];
+                        var bytesToCopy = Math.Min(buffer.Length, (int)(fileSize - copiedBytes));
 
-                        do
-                        {
-                            len = fsRead.Read(buffer, 0, buffer.Length); // 8
-                            fsWrite.Write(buffer, 0, len);
+                        fsRead.Read(buffer, 0, bytesToCopy);
+                        fsWrite.Write(buffer, 0, bytesToCopy);
 
-                            fileSize -= len;
+                        copiedBytes += bytesToCopy;
+                        var progress = (int)(copiedBytes * 100 / fileSize);
+                        Dispatcher.Invoke(() => Progressbar.Value = progress);
 
-                            Thread.Sleep(100);
+                        Thread.Sleep(500);
+                    } while (copiedBytes < fileSize);
 
-                        } while (len != 0);
-
-                    }
                 }
-            });
+            }
+            Dispatcher.Invoke(() => btnCopy.IsEnabled = true);
+        }
 
-            
+        private void btnPauseAndResume_Click(object sender, RoutedEventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Unstarted)
+                return;
+
+            isStopped = !isStopped;
+
+            if (isStopped)
+            {
+                btnPauseAndResume.Content = "Resume";
+
+                try
+                {
+                    thread.Suspend();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+            else
+            {
+                btnPauseAndResume.Content = "Pause";
+
+                try
+                {
+                    thread.Resume();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Unstarted)
+                return;
+
+            thread.Abort();
+
+            if (File.Exists(DestinationPath))
+                File.Delete(DestinationPath);
+
+            btnCopy.IsEnabled = true;
+            Progressbar.Value = 0;
         }
     }
 }
